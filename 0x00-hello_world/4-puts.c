3d31176f283ba9ee6cd5eeb3b5acc67970e80057
NickyG-sys <nickygouws21@gmail.com>
2022-07-01 09:52:33 -0700

    low_level_programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,18 @@
-#include <stdio.h>
 /**
- *  * main - main block
- *   * Return: 0
- *    */
+ *File name : 4-puts.c
+ *Author : Nicky Gouws
+ */
+
+#include <stdio.h>
+
+/**
+ *  * main - Prints "Programming is like building a multilingual
+ *   *                puzzle, followed by a new line."
+ *    *
+ *     * Return: Always 0.
+ *      */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
-
