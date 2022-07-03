c1f67974cdf9e38c12924e52bae055b48f19d45e
NickyG-sys <nickygouws21@gmail.com>
2022-07-03 14:30:51 -0700

    variables_if_else_while

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- *  * main - Entry point
- *   *
- *    * Return: Always 0 (Success)
- *     */
+ * main - entry point
+ *
+ * Return: 0 (success)
+ */
 int main(void)
 {
 char alphabet;
