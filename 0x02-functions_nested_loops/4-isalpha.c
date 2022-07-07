5af2aa35a337225a988eb5e1cf0b01061224d108
NickyG-sys <nickygouws21@gmail.com>
2022-07-06 21:56:32 -0700

    is_alpha updated

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ *_isalpha - checks for alphabetical letters
+ @c: a charter to be checked on
+ *Return: return 0 and 1 depending on condition
+ */
+int _alpha(int c)
+{
+	return ((c >= 'a' && <= 'z') || (c >= 'A' && c <= 'Z'));
+}
