fae590c89f5daf84f007b3387663b2f40ca42e89
NickyG-sys <nickygouws21@gmail.com>
2022-07-09 23:33:14 -0700

    nested loops

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,13 +3,17 @@
 /**
  * print_numbers - prints the numbers, from 0 to 9,
  * followed by a new line
+ * Return: Always 0.
  */
 void print_numbers(void)
 {
-int x;
+int i;
 
-for (x = 0; x < 10; x++)
-	_putchar(x + '0');
-_putchar('\n');
-
+i = 0;
+while (i < 10)
+{
+	_putchar (i +'0');
+	i++;
+}
+	_putchar ('\n');
 }
