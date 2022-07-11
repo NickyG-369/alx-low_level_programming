9f9404408ea472568a4db8feb6c9f28153c2d646
NickyG-sys <nickygouws21@gmail.com>
2022-07-11 07:31:13 -0700

    pointers_arrays

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,14 +1,19 @@
 #include "main.h"
+#include <stdio.h>
+
 /**
 *swap_int -. given two intergers swap the values they are holding
 * @: parameter  1
 * @b: parameter 2
 * Return: Always 0 (success)
+* Description: This function takes a pointer to an int as parameter
+*  and updates the value it points to to 98.
 */
+
 void swapint(int *a, int *b)
 {
-	
-	int tmp = *a;
-	*a = *b;
-	*b = tmp;
+int temp = *a;
+
+*a = *b;
+*b = temp;
 }
