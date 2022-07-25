07e81d2c7d670eb944d50076b9f744d77d2c3bce
NickyG-sys <nickygouws21@gmail.com>
2022-07-25 08:39:15 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main -> this is a function
+ * @argc: argc parameter
+ * @argv: an array of a command listed
+ * Return: 0 foe success
+ */
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
