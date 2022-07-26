b14ebeaddf6c18e91febcfd7a09c7a23a3063121
NickyG-sys <nickygouws21@gmail.com>
2022-07-26 04:25:50 -0700

    updated

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 /**
- * main -> this is a function
- * @argc: argc parameter
- * @argv: an array of a command listed
- * Return: 0 foe success
- */
-int main(int __attribute__((__unused__)) argc, char *argv[])
+ * main -  writes its name
+ * @argc: arguement count
+ * @argv: array of pointers to arguement strings
+ * Return: 0
+ **/
+int main(int argc, char *argv[])
 {
-	printf("%s\n", argv[0]);
-
+	argc = argc;
+	printf("%s\n", *argv);
 	return (0);
 }
