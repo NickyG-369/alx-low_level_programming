11fa3c3964340daea9167f5b5f78c04c4c86f887
NickyG-sys <52282761+NickyG-sys@users.noreply.github.com>
2022-08-11 21:35:46 +0200

    Update 2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,38 +1,29 @@
 #include "lists.h"
-#include <string.h>
 /**
- *add_node - function to add item at the beginning of a linked_list
- *@head: head pointer to linked_list
- *@str: string to add
- *
- *Return: address of new element
+ * add_node - adds a new node at the beginning
+ * of a list_t list.
+ * @head: head of the linked list.
+ * @str: string to store in the list.
+ * Return: address of the head.
  */
+
 list_t *add_node(list_t **head, const char *str)
 {
-list_t *new_node;
-unsigned int i = 0;
+	list_t *new;
+	size_t nchar;
 
-if (str == NULL)
-return (NULL);
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
 
-new_node = malloc(sizeof(list_t));
-if (new_node == NULL)
-return (NULL);
+	new->str = strdup(str);
 
-new_node->str = strdup(str);
-if (new_node->str == NULL)
-{
-free(new_node);
-return (NULL);
-}
+	for (nchar = 0; str[nchar]; nchar++)
+		;
 
-while (new_node->str[i])
-{
-i++;
-}
-new_node->len = i;
-new_node->next = *head;
-*head = new_node;
+	new->len = nchar;
+	new->next = *head;
+	*head = new;
 
-return (new_node);
+	return (*head);
 }
